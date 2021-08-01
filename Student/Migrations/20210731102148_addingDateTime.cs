41d520e8d1eb723dbc4c54033480225ff68e7ec7
adavuruku <aabdulraheemsherif@gmail.com>
2021-08-01 02:13:43 +0100

    configure jwq

---
+++ Student/Migrations/20210731102148_addingDateTime.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Student.Migrations
+{
+    public partial class addingDateTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Teacher",
+                type: "timestamp without time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Teacher",
+                type: "timestamp without time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "StudentAddress",
+                type: "timestamp without time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "StudentAddress",
+                type: "timestamp without time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Student",
+                type: "timestamp without time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Student",
+                type: "timestamp without time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Standard",
+                type: "timestamp without time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Standard",
+                type: "timestamp without time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Products",
+                type: "timestamp without time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Products",
+                type: "timestamp without time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Grade",
+                type: "timestamp without time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Grade",
+                type: "timestamp without time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Course",
+                type: "timestamp without time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Course",
+                type: "timestamp without time zone",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Teacher");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Teacher");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "StudentAddress");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "StudentAddress");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Student");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Student");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Standard");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Standard");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Grade");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Grade");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Course");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Course");
+        }
+    }
+}
---
+++ Student/Migrations/20210731223741_addingStudentPassworAndRegno.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Student.Migrations
+{
+    public partial class addingStudentPassworAndRegno : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "StudentPassword",
+                table: "Student",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StudentRegNo",
+                table: "Student",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StudentPassword",
+                table: "Student");
+
+            migrationBuilder.DropColumn(
+                name: "StudentRegNo",
+                table: "Student");
+        }
+    }
+}
