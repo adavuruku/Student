20d0b5337f0b67b37813b1e40fcd12cf44562652
adavuruku <aabdulraheemsherif@gmail.com>
2021-08-02 01:32:33 +0100

    done

---
+++ Student/Migrations/20210802001612_AddRolesTables.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Student.Migrations
+{
+    public partial class AddRolesTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Role",
+                columns: table => new
+                {
+                    roleId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    roleTitle = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Role", x => x.roleId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RoleStudent",
+                columns: table => new
+                {
+                    RolesroleId = table.Column<int>(type: "integer", nullable: false),
+                    StudentsStudentID = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoleStudent", x => new { x.RolesroleId, x.StudentsStudentID });
+                    table.ForeignKey(
+                        name: "FK_RoleStudent_Role_RolesroleId",
+                        column: x => x.RolesroleId,
+                        principalTable: "Role",
+                        principalColumn: "roleId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RoleStudent_Student_StudentsStudentID",
+                        column: x => x.StudentsStudentID,
+                        principalTable: "Student",
+                        principalColumn: "StudentID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoleStudent_StudentsStudentID",
+                table: "RoleStudent",
+                column: "StudentsStudentID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RoleStudent");
+
+            migrationBuilder.DropTable(
+                name: "Role");
+        }
+    }
+}
